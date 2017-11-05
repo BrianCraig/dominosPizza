d4039b609e22289ea3177254c072132d33e2ebd7
Brian Craig <briancraigok@gmail.com>
2017-11-05 12:45:47 -0300

    repos

---
+++ angular/app/domain/tamanio.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Faltan ingredientes
+
+class Tamanio {
+    constructor(objeto) {
+        this.id = objeto.id;
+        this.precio = objeto.precio;
+        this.nombre = objeto.nombre;
+    }
+}
